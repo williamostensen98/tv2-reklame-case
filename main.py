0ac67c07ac19810a4d7c20f1dfc0fc9a95e6874e
William Østensen <william-98-@hotmail.com>
2026-05-10 19:00:53 +0200

    Solution and rquirements file

---
+++ main.py
@@ -0,0 +1,40 @@
+import json
+from fastapi import FastAPI, Request, Response, HTTPException
+from fastapi.responses import JSONResponse
+from confluent_kafka import Producer
+
+
+producer_config = {'bootstrap.servers': 'localhost:9094'}
+app = FastAPI()
+producer = Producer(producer_config)
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.value().decode('utf-8')}")
+
+@app.post("/userdata")
+async def process_data_to_kafka(request: Request):
+
+    try:
+        data = await request.json()
+
+    except Exception:
+        raise HTTPException(status_code=400, detail=f"Invalid JSON")
+
+    try:
+        producer.produce(
+            topic="user-data",
+            value=json.dumps(data).encode("utf-8"),
+            callback=delivery_report
+        )
+        producer.flush()
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
+
+    return JSONResponse(status_code=200, content="200 OK")
